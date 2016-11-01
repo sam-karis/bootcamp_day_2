4d81f598dec999ff81b582de4bfe80bf6a6c67f5
unknown <samkaris75@gmail.com>
2016-11-01 11:00:11 +0300

    file has a words function that count the number of words to return a dictionary with word as keys and number occurence as values it is modified from previous to remove repetation in function

--- words.py
+++ words.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 import os, sys
 
@@ -8,22 +8,13 @@ def words(string_word):
 	split_word = split_word.replace("\t", " ")
 	split_word = split_word.replace("  ", " ")
 	split_word = split_word.split(" ")
-	#print split_word
 	word_count = {}
 	for item in split_word:
 		if item.isdigit() == True:
 			item = int(item)
-			if item in word_count:
+		if item in word_count:
 				word_count[item] += 1
-			else:
-				word_count[item] = 1
 		else:
-			if item in word_count:
-				word_count[item] += 1
-			else:
-				word_count[item] = 1
+			word_count[item] = 1
 
 	return word_count
-
-print (words("Nairobi is a city of\ncity city  1 2 Nairobi"))
-print (words('¡Hola! ¿Qué tal? Привет!'))
